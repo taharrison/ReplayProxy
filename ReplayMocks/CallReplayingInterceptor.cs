0263eaeab1cf3972270f5383fb4ee22e5635f86e
taharrison <taharrison@gmail.com>
2014-05-28 08:39:29 +0100

    initial commit / proof of concept : can record / playback

---
+++ ReplayMocks/CallReplayingInterceptor.cs
@@ -0,0 +1,101 @@
+﻿using System.Text;
+using Castle.DynamicProxy;
+
+namespace ReplayMocks
+{
+    public class CallReplayingInterceptor : IInterceptor
+    {
+        public History Log { get; set; }
+        public bool AllowAccessToObject { get; set; }
+
+        public CallReplayingInterceptor(History history)
+        {
+            AllowAccessToObject = false;
+            Log = history;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            Log.ReplayMethodResult(invocation);
+            
+        }
+
+        //private void AppendMethodCallToLog(IInvocation invocation)
+        //{
+        //    _log.Append("Method Call: ");
+        //    _log.Append(invocation.Method);
+        //    _log.Append("(");
+        //    var arguments = String.Join(", ", invocation.Arguments.Select(x => EncodeArgument(x)).ToArray());
+        //    _log.Append(arguments);
+        //    _log.AppendLine(");");
+        //}
+
+        //private string Stringify(object argument)
+        //{
+        //    if (argument == null)
+        //    {
+        //        return "{null}";
+        //    }
+        //
+        //    var allowedTypes = new[] { typeof(string), typeof(byte), typeof(Array), typeof(IList) };
+        //    if (!allowedTypes.Any(x => x.IsInstanceOfType(argument)))
+        //    {
+        //        throw new NotImplementedException("need to ensure we can uniquely serialise this type in the callloggingintercetpor " + argument.GetType().ToString());
+        //    }
+        //
+        //    if (argument is Array)
+        //    {
+        //        var sb = new StringBuilder();
+        //        sb.Append("Array");
+        //
+        //        return serialiseIEnumerable(argument, sb);
+        //    }
+        //
+        //    if (argument is IList)
+        //    {
+        //        var sb = new StringBuilder();
+        //        sb.Append("List");
+        //
+        //        return serialiseIEnumerable(argument, sb);
+        //    }
+        //
+        //    return Encode(argument.ToString());
+        //}
+
+        //private string serialiseIEnumerable(object argument, StringBuilder sb)
+        //{
+        //    sb.Append("[");
+        //    var isFirst = true;
+        //    var asiEnumerable = argument as IEnumerable;
+        //    foreach (var item in asiEnumerable)
+        //    {
+        //        if (!isFirst)
+        //        {
+        //            sb.Append(", ");
+        //        }
+        //        sb.Append(Stringify(item));
+        //        isFirst = false;
+        //    }
+        //    sb.Append("]");
+        //    return sb.ToString();
+        //}
+
+        //private string Encode(string input)
+        //{
+        //    return input.Replace(@"\", @"\\")
+        //        .Replace(@"{", @"\{")
+        //        .Replace(@"}", @"\}")
+        //        .Replace(@"[", @"\[")
+        //        .Replace(@"]", @"\]")
+        //        .Replace(@",", @"\,")
+        //        .Replace("\n", "\\n")
+        //        .Replace("\r", "\\r");
+        //}
+
+        //private string EncodeArgument(object argument)
+        //{
+        //    var asText = Stringify(argument);
+        //    return "{" + asText + "}";
+        //}
+    }
+}
---
+++ ReplayMocks/XmlSerialiser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ReplayMocks
+{
+    public class XmlSerialiser
+    {
+
+        public static object Deserialise(string xml, Type type)
+        {
+            var xmlSerializer = new XmlSerializer(type);
+            var stream = new StringReader(xml);
+
+            return xmlSerializer.Deserialize(stream);
+        }
+        public static string Serialise(object model, Type type)
+        {
+            var xmlSerializer = new XmlSerializer(type);
+            var sb = new StringBuilder();
+            var stream = new StringWriter(sb);
+            xmlSerializer.Serialize(stream, model);
+            return sb.ToString();
+        }
+
+        public static string SerialiseWithoutNamespacesAndHeaderWithLinebreaks(object model, Type type, string xmlRoot = null)
+        {
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add(string.Empty, string.Empty);
+
+            var xmlSerializer = xmlRoot != null ? new XmlSerializer(type, new XmlRootAttribute(xmlRoot)) : new XmlSerializer(type);
+            var sb = new StringBuilder();
+            using (XmlWriter stream = XmlWriter.Create(sb, new XmlWriterSettings() { OmitXmlDeclaration = true, Indent = true }))
+            {
+                xmlSerializer.Serialize(stream, model, ns);
+            }
+            return sb.ToString();
+        }
+
+        public static string Serialise<T>(T model, string xmlRoot)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRoot));
+            var sb = new StringBuilder();
+            var stream = new StringWriter(sb);
+            xmlSerializer.Serialize(stream, model);
+            return sb.ToString();
+        }
+    }
+}
